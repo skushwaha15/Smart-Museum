2a0d4cef9681c301d014f8e37fa7e4f7ff1533f6
skushwaha15 <sejalkushwaha342@gmail.com>
2026-04-06 02:37:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ const query = async (text, params) => {
 // ==================== EMAIL CONFIGURATION - BREVO (SECURE) ====================
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
+    port: 2525,
+    secure: true,
     auth: {
         user: process.env.BREVO_EMAIL,
         pass: process.env.BREVO_SMTP_KEY
