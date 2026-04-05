a31ded8891be8b8e4771e2a4317070869912cf54
skushwaha15 <sejalkushwaha342@gmail.com>
2026-04-05 20:15:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.BREVO_EMAIL,
         pass: process.env.BREVO_SMTP_KEY
     },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
     debug: true,  // Enable debug logging
     logger: true  // Enable logger
 });
