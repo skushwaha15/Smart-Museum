d83ebc85dd006fa269e71e7162877e020934a1d5
skushwaha15 <sejalkushwaha342@gmail.com>
2026-04-06 01:20:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,6 +71,23 @@ const transporter = nodemailer.createTransport({
     }
 })();
 
+app.get("/test-email", async (req, res) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `"Test" <a73020001@smtp-brevo.com>`,
+            to: "sejalkushwaha342@gmail.com",
+            subject: "TEST EMAIL",
+            text: "Hello from server"
+        });
+
+        console.log("SUCCESS:", info);
+        res.send("Email sent");
+    } catch (err) {
+        console.error("ERROR:", err);
+        res.send("Failed");
+    }
+});
+
 
 
 let adminOtpStore = {};
